ee4360c41e65cde8d1c2d78930939101640d1713
Ravi Kanodia <ravileonkanodia@gmail.com>
2017-04-19 09:10:44 -0700

    :construction: Parse CSV files into lookup tables

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 
 const fs = require('fs');
 const ArgumentParser = require('argparse').ArgumentParser; 
+const EntityTable = require('./lib/EntityTable');
+const _ = require('underscore');
 
 var argParser = new ArgumentParser({
     version: '0.0.0',
     addHelp: true,
     description: 'Track blood sugar based on food and exercise.'
   });
-
 argParser.addArgument(
     ['-f', '--food'],
     {
@@ -39,3 +40,14 @@ var inputFileBuffer = fs.readFileSync(args.input);
 console.log('food file: ' + foodFileBuffer.toString());
 console.log('exercise file: ' + exerciseFileBuffer.toString());
 console.log('input file: ' + inputFileBuffer.toString());
+
+var foodEntityTable = EntityTable.FoodTable(foodFileBuffer);
+var exerciseEntityTable = EntityTable.ExerciseTable(exerciseFileBuffer);
+console.log('food table: ' + foodEntityTable.name);
+_.each(foodEntityTable.entities, function(value, key) {
+  console.log(value.name + ': ' + key + ', ' + value.value + ', ' + value.duration);
+});
+console.log('exercse table: ' + exerciseEntityTable.name);
+_.each(exerciseEntityTable.entities, function(value, key) {
+  console.log(value.name + ': ' + key + ', ' + value.value + ', ' + value.duration);
+});
